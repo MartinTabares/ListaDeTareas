ea2d19e183b9a2a45db917ab9e11b99ec6c5f0a0
Martin <martinalejandrotabares@gmail.com>
2020-08-17 20:04:59 -0300

    commit inicial del proyecto de tareas

---
+++ vue.js
@@ -0,0 +1,71 @@
+var data = {
+    titulo: 'Lista de Tareas',
+    nuevaTarea: '',
+    tareas: [{
+            objetivo: 'Trabajar en Silstech',
+            terminada: false
+        },
+        {
+            objetivo: 'Cambiar el auto',
+            terminada: false
+        },
+        {
+            objetivo: 'Agrandar la casa',
+            terminada: false
+        }
+    ]
+
+}
+Vue.component('titulo', {
+    template: `<h1><strong>{{titulo}}</strong></h1>`,
+    data: function (titulo) {
+        return  data;
+    }
+})
+Vue.component('agregar-tareas', {
+    template: `<div class="input-group">
+                <input type="text" v-model='nuevaTarea' class="form-control" placeholder="Escribe una nueva tarea" v-on:keyup.enter='agregarTarea'>
+                <button v-on:click='agregarTarea' class="btn btn-primary">Agregar</button>
+                </div>`,
+    data: function(){
+        return data;
+    },
+    methods: {
+        agregarTarea: function () {
+            let objetivo = this.nuevaTarea.trim();
+            let terminada = false;
+            if (objetivo) {
+                this.tareas.push({
+                    objetivo,
+                    terminada
+                })
+            }
+            this.nuevaTarea = '';
+        }
+    }            
+})
+Vue.component('lista-de-tareas', {
+    template: `<ul class="list-group">
+                    <li v-for='(tarea, indice) of tareas' class="list-group-item d-flex justify-content-between align-items-center" v-bind:class="{listo: tarea.terminada}">{{tarea.objetivo}}
+                        <span>
+                            <button class="btn btn-success" v-on:click='tarea.terminada = !tarea.terminada'>Lista</button>
+                            <button class="btn btn-danger" v-on:click='borrar(indice)'>Borrar</button>
+                        </span>
+                    </li>
+                </ul>`,
+    data: function(){
+        return data;
+    },
+    methods: {
+        borrar: function (indice) {
+            this.tareas.splice(indice, 1);
+        }
+    }
+
+})
+
+var app = new Vue({
+    el: '#app',
+    data: data,
+
+})
